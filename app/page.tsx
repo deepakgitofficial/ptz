fc85d979fdce3f796deed09f4f2400475d53a55a
voucherinfinity <voucherinfinity9@gmail.com>
2026-06-11 18:38:13 +0530

    home-issue

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,6 @@ import FaqSection from "@/components/home/FaqSection";
 import BusinessDomains from "@/components/home/BusinessDomains";
 
 import { client } from "@/lib/sanity";
-import imageUrlBuilder from "@sanity/image-url";
-const builder = imageUrlBuilder(client);
-
-function urlFor(source) {
-  return builder.image(source);
-}
 
 export default   async function  Home() {
 const projectData = await client.fetch(`*[_type == "ProjectsSchema"]`);
